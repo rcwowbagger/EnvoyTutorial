cc9635d9cb81a28fa16bf183058fa367aac8b5b7
Richard Cupples <rcupples@outlook.com>
2020-10-27 23:53:21 +0100

    Add project files.

---
+++ CoffeeApi/Controllers/CoffeeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace CoffeeApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CoffeeController : ControllerBase
+    {
+        private static readonly string[] Coffees = new[]
+        {
+            "lungo", "espresso", "ristretto"
+        };
+
+        private readonly ILogger<CoffeeController> _logger;
+
+        public CoffeeController(ILogger<CoffeeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var rng = new Random();
+            return Ok(Coffees[rng.Next(Coffees.Length)]);
+        }
+    }
+}
---
+++ TeaApi/Controllers/TeaController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace TeaApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TeaController : ControllerBase
+    {
+        private static readonly string[] Teas = new[]
+        {
+            "Black", "Green", "Assam"
+        };
+
+        private readonly ILogger<TeaController> _logger;
+
+        public TeaController(ILogger<TeaController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var rng = new Random();
+            return Ok(Teas[rng.Next(Teas.Length)]);
+
+        }
+    }
+}
